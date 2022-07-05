988f710b8b3aef5f8ffdf0942cdbb6955f3aab91
TwistedTaleTeller <wojciechmajewski330@gmail.com>
2022-07-05 16:48:43 +0200

    Second solution

--- Plakatowanie/solution.cpp
+++ Plakatowanie/solution.cpp
@@ -5,35 +5,40 @@
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
-	int N, output = 0, temp;
+	int N, output = 0, temp, i, j;
 	bool is_redundant;
 	std::vector <int> building_heights;
-	building_heights.push_back(0); // simplifies the comparing process
+	std::vector <bool> building_redundant;
 	std::cin >> N;
 
-	for (int i = 0; i < N; i++)
+	for (i = 0; i < N; i++)
 	{
 		std::cin >> temp >> temp; // building length is unimportant for this task, just the heights
 		building_heights.push_back(temp);
+		building_redundant.push_back(false);
+	}
 
-		is_redundant = false;
-		for (int j = building_heights.size() - 2; j >= 0; j--)
+	for (i = 0; i < N; i++)
+	{
+		if (building_redundant[i]) continue;
+
+		output++;
+
+		for (j = i + 1; j < N; j++)
 		{
-			if (building_heights[j] < building_heights.back())
+			if (building_heights[j] < building_heights[i])
 			{
 				break;
 			}
-			
-			if (building_heights[j] == building_heights.back())
+			if (building_heights[j] == building_heights[i])
 			{
-				is_redundant = true;
-				break;
+				if (building_redundant[j]) // Already outclassed these buildings
+				{
+					break;
+				}
+				building_redundant[j] = true;
+				continue;
 			}
-		}
-
-		if (!is_redundant)
-		{
-			output++;
 		}
 	}
 	
