edc16441a9fe51cc0af5c7e9c8082bd99242b478
TwistedTaleTeller <wojciechmajewski330@gmail.com>
2022-07-05 18:37:46 +0200

    Complete solution

--- Plakatowanie/solution.cpp
+++ Plakatowanie/solution.cpp
@@ -5,40 +5,29 @@
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
-	int N, output = 0, temp, i, j;
-	bool is_redundant;
 	std::vector <int> building_heights;
-	std::vector <bool> building_redundant;
+	std::vector <int> poster_stack;
+	int N, output = 0, temp;
 	std::cin >> N;
 
-	for (i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		std::cin >> temp >> temp; // building length is unimportant for this task, just the heights
+		/* building length is unimportant for this task, just the heights */
+		std::cin >> temp >> temp;
 		building_heights.push_back(temp);
-		building_redundant.push_back(false);
 	}
 
-	for (i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		if (building_redundant[i]) continue;
-
-		output++;
-
-		for (j = i + 1; j < N; j++)
+		while (!poster_stack.empty() && poster_stack.back() > building_heights[i])
 		{
-			if (building_heights[j] < building_heights[i])
-			{
-				break;
-			}
-			if (building_heights[j] == building_heights[i])
-			{
-				if (building_redundant[j]) // Already outclassed these buildings
-				{
-					break;
-				}
-				building_redundant[j] = true;
-				continue;
-			}
+			poster_stack.pop_back();
+		}
+
+		if (poster_stack.empty() || poster_stack.back() < building_heights[i])
+		{
+			poster_stack.push_back(building_heights[i]);
+			output++;
 		}
 	}
 	
